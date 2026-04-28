badbf73e32088005f68617bb97f8e4294a1c3a66
Uttpal Singh <uttpalsingh8@gmail.com>
2026-04-28 13:44:56 +0000

    DataTypes

---
+++ python3.py
@@ -0,0 +1,7 @@
+print('Datatypes')
+# Numbers : 1234,4.4232,3+8k,0b111,Decimal(),Fraction()
+# String : "span", "kohli's", b'a/x01fc',
+# List : [1,2,3,4,5],list()
+# Tuple : (1,'kohli',5,'virat'), tuple()
+# Dictionary : {'food':'chole','taste:'yum'}, dict()
+
