a138eb42ea45aff195b13fd9dbb48492b6fd8d4e
Uttpal Singh <uttpalsingh8@gmail.com>
2026-04-28 14:11:59 +0000

    String

--- python4.py
+++ python4.py
@@ -1,10 +1,10 @@
 print('string')
 
 username = "runmachine"
-print(len(username)) 
+print(len(username))  #10
 print(username[0])    #r
 # username[0] = 'A'
 # print(username) -> ERROR (because username is immutable and you can't change any immutable data)
-print(username[-1])
+print(username[-1])  #e
 
 print(username[1:4])   #unm (slicing)
