7a036b729e701936c478dc007cb0d0141b3e0de2
Uttpal Singh <uttpalsingh8@gmail.com>
2026-04-28 13:32:14 +0000

    Immutable

---
+++ python2.py
@@ -0,0 +1,10 @@
+print("mutable and immurable")
+
+username = "Virat"
+print(username)
+username = "kohli "
+print(username)
+
+# username is string and string is immutable.
+# Here we pass the reference of username to kohli from Virat but actually there is no change is username.
+# because of automatic garbage collection in python Virat will be deleted from the memory automatically and new value of username will be kohli.
