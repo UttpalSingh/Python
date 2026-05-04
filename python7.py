f95c0f63424f680443202bfa53457c78bc9579de
Uttpal Singh <uttpalsingh8@gmail.com>
2026-05-04 11:56:17 +0000

    numbers

---
+++ python7.py
@@ -0,0 +1,19 @@
+import math
+import random
+
+# print(math.floor(3.5))
+# print(math.floor(3.9))
+# print(math.trunc(2.8))
+# print(math.trunc(-2.8))
+
+# print(int(64.34))
+# print(int('64',8)) # convert into octal
+# print(int('64',16)) # convert into hexadecimal
+# print(int('100000',2)) # convert into binary
+
+# print(random.randint(1,100)) # give random value between 1 to 100
+# L1 = ["CaptainAmerica","IronMan","SpiderMan","Hulk","Thor"]
+# print(random.choice(L1)) # choose random value 
+
+
+
