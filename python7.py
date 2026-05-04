02ab3b9847e833c150017ce1b1dfed1ddfe83c48
Uttpal Singh <uttpalsingh8@gmail.com>
2026-05-04 12:04:29 +0000

    Numbers

--- python7.py
+++ python7.py
@@ -1,6 +1,7 @@
 import math
 import random
 
+# math
 # print(math.floor(3.5))
 # print(math.floor(3.9))
 # print(math.trunc(2.8))
@@ -11,14 +12,17 @@ import random
 # print(int('64',16)) # convert into hexadecimal
 # print(int('100000',2)) # convert into binary
 
+
+# random
 # print(random.randint(1,100)) # give random value between 1 to 100
 # L1 = ["CaptainAmerica","IronMan","SpiderMan","Hulk","Thor"]
 # print(random.choice(L1)) # choose random value 
 
-mySets = {1,2,3,4}
-print(mySets & {1,3})  # intersection {1, 3}
-print(mySets | {1,3})  # Union {1, 2, 3, 4}
+# sets
+# mySets = {1,2,3,4}
+# print(mySets & {1,3})  # intersection {1, 3}
+# print(mySets | {1,3})  # Union {1, 2, 3, 4}
 
-setOne = mySets - {1,2,3,4}
-print(setOne)  # set()
-print(type(setOne)) # <class 'set'>
+# setOne = mySets - {1,2,3,4}
+# print(setOne)  # set()
+# print(type(setOne)) # <class 'set'>
