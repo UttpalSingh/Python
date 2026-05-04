951b7c4dbb04f5fab7cfe62123fbca1749e032a7
Uttpal Singh <uttpalsingh8@gmail.com>
2026-05-04 12:51:08 +0000

    String

--- python4.py
+++ python4.py
@@ -1,10 +1,62 @@
-print('string')
+print('String in Python')
 
-username = "runmachine"
-print(len(username))  #10
-print(username[0])    #r
-# username[0] = 'A'
-# print(username) -> ERROR (because username is immutable and you can't change any immutable data)
-print(username[-1])  #e
+# username = "runmachine"
+# print(len(username))  #10
+# print(username[0])    #r
+# # username[0] = 'A'
+# # print(username) -> ERROR (because username is immutable and you can't change any immutable data)
+# print(username[-1])  #e
 
-print(username[1:4])   #unm (slicing)
+# #Slicing in Python
+# print(username[1:4])  #unm 
+# name = "Virat Kohli"
+# slice_name = name[0:6]
+# print(slice_name) # Virat 
+# ##################
+# numList = "0123456789"
+# print(numList[:]) # 0123456789
+# print(numList[1:]) # 123456789
+# print(numList[:11]) # 0123456789
+# print(numList[0:11]) # 0123456789
+# print(numList[0:7:2]) # 0246
+# print(numList[0:11:3]) # 0369
+
+
+# newName = "KingKohli"
+# print(newName.lower())
+# print(newName.upper())
+# print(newName.strip())
+
+# hero = "Captain America"
+# print(hero.replace("Captain","Naughty")) 
+
+# newHero = "CaptainAmerica,IronMan,BatMan,MoonKnight"
+# print(newHero.split()) # ['CaptainAmerica,IronMan,BatMan,MoonKnight']
+# print(newHero.split(",")) #  ['CaptainAmerica', 'IronMan', 'BatMan', 'MoonKnight']
+# print(newHero.find("MoonKnight")) # 30
+# print(newHero.count("MoonKnight")) # 1
+
+# # substring in python
+# hero_type = "Alpha"
+# quantity = 4
+# work = f"I want to be {hero_type} hero and there are total {quantity} hero in newHero"
+# print(work) # I want to be Alpha hero and there are total 4 hero in newHero
+# Work = "I want to be {} hero and there are total {} hero in newHero"
+# print(Work.format(hero_type,quantity)) # I want to be Alpha hero and there are total 4 hero in newHero
+
+
+# Language = ["French","Spanish","English","Arabic"]
+# print(type(Language)) # <class 'list'>
+# print(len(Language)) # 4
+# newLanguage = " ".join(Language)
+# print(newLanguage) # French Spanish English Arabic
+# for lang in Language:
+#     print(lang) 
+# #French
+# #Spanish
+# #English
+# #Arabic
+
+# letter = "She said, \"You are a good programmer\" "
+# print(letter) # She said, "You are a good programmer" 
+
