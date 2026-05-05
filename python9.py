a4857990d7d8c233f3a2c0e6cc5aa61255fe27ab
Uttpal Singh <uttpalsingh8@gmail.com>
2026-05-05 12:55:19 +0000

    Dictionary

---
+++ python9.py
@@ -0,0 +1,38 @@
+print("Dictionary in Python")
+
+# Notes:-
+# Dictionary is mutable.(line16)
+# .get() is used to acess data by using key.(line18)
+# .pop() is used to delete data from the dict.(line22)
+
+
+Player_Details = {
+    "name" : "Virat",
+    "ID" : 18,
+    "CallSign" : "Chiku",
+    "Player" : "GOAT"
+}
+# print(Player_Details) #{'name': 'Virat', 'ID': 18, 'CallSign': 'Chiku', 'Player': 'GOAt'}
+# print(Player_Details["name"]) # Virat
+# Player_Details["name"] = "KingKohli" 
+# print(Player_Details["name"]) # KingKohli
+
+# Methods in Dictionay
+# print(Player_Details.get("ID")) # 18
+# Player_Details.pop("CallSign") # this method is used to delete data from the dict by the key name
+# print(Player_Details) # {'name': 'Virat', 'ID': 18, 'Player': 'GOAT'}
+
+
+# # Loop on dictionary
+# for i in Player_Details:
+    # print(i) # it gives only key name
+    # print(i,Player_Details[i]) # it gives value with key name
+
+# for key,value in Player_Details.items(): # By this we can print both key and value 
+    # print(key) # it gives key 
+    # print(value) # it gives value 
+    # print(key,value) # it gives both key and value
+
+# # Add value in dictionay
+# Player_Details["Type"]  = "Batsman" 
+# print(Player_Details) # {'name': 'Virat', 'ID': 18, 'CallSign': 'Chiku', 'Player': 'GOAT', 'Type': 'Batsman'}
