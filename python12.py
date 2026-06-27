661fdb20c29c0274e3a006d0b675a4b727ebaddc
Uttpal Singh <uttpalsingh8@gmail.com>
2026-06-27 15:27:14 +0000

    Problems in Function

--- python12.py
+++ python12.py
@@ -2,6 +2,12 @@ import math
 
 #Problems in Function
 
+#Notes
+#lambda function(60)
+#Format String(51,52,92)
+#Add multiple value in function(64)[function with *args]
+#Print multiple parameter and argument inside a function(89)[function with **kwargs]
+
 
 #Problem1
 def calculateSquare(num):
@@ -40,7 +46,50 @@ def parm_of_circle(radius):
     area = math.pi * radius ** 2
     return area,circumference
 
-num = int(input("enter a number : "))
-a,c = parm_of_circle(num)
-print(f"Area : {a:2f}")
-print(f"Circumference : {c:2f}")
+# num = int(input("enter a number : "))
+# a,c = parm_of_circle(num)
+# print(f"Area : {a:2f}")
+# print(f"Circumference : {c:2f}")
+
+# Problem5
+def greet(name = "Raj"): # function with default parameter
+    return "Hi" + " " + name + " " + "You are welcome here"
+
+# print(greet("VIGI"))
+
+#Probelm6
+cube = lambda x: x ** 3
+# print(cube(3))
+
+#Problem7 
+def sum_all(num):
+    sum = 0
+    for i in num:
+        sum += i
+    return sum
+
+# n = int(input("enter a number to run number of loop : "))
+
+list = []
+
+# for i in range(1,n+1):
+    # num = int((input("Enter a number : ")))
+    # list += [num]
+# print("sum = ",sum_all(list)) # here I store the value list in function
+
+def sum_All(*args):
+    print(*args) # 1 2 3 4
+    print(args) # (1, 2, 3, 4)
+    for i in args:
+        print (i * 2) # 2,4,6,8
+
+    return sum(args)
+# print("sum = ",sum_All(1,2,3,4))
+
+#Problem8
+def printKwargs(**kwargs):
+    for key,value in kwargs.items():
+        print(f"{key} : {value}")
+# print(printKwargs(name = "virat",role = "Batsman",nickname = "Chiku"))
+
+#Problem9
