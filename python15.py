9ecc6b5b6de39aae0dc8ce3ac63bddff78f941a3
Uttpal Singh <uttpalsingh8@gmail.com>
2026-06-30 11:24:09 +0000

    Project

--- python15.py
+++ python15.py
@@ -12,13 +12,17 @@ def load_data():
 def save_data(videos):
     with open('youtube.txt','w') as file:
         json.dump(videos,file)
-        
+
 
 def list_all_videos(videos):
-    pass
+    for index,video in enumerate(videos,start=1):
+        print(f"{index}. ")
 
 def add_video(videos):
-    pass
+    name = input("Enter video name: ")
+    time = input("Enter video time: ")
+    videos.append({'name':name,'time':time})
+    save_data(videos)
 
 def update_video(videos):
     pass
@@ -37,6 +41,7 @@ def main():
         print("4. Delete a youtube video")
         print("5. Exit the app")
         choice = input("Enter your choice : ")
+        print(videos)
 
         match choice:
             case '1':
