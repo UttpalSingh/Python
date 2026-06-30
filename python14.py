d4af1cfad7051a3c7fef21d820168b0903c217b7
Uttpal Singh <uttpalsingh8@gmail.com>
2026-06-30 09:32:36 +0000

    Decorators in Python

--- python14.py
+++ python14.py
@@ -19,3 +19,17 @@ def example_function(n):
 # example_function(2)#example_function ran in 2.0001697540283203 time
 
 #Problem2(Debugging Function Calls)
+def debug(func):
+    def wrapper(*args,**kwargs):
+        args_value = ', '.join(str(arg) for arg in args)
+        kwargs_value = ', '.join(f"{k}={v}"for k,v in kwargs.items())
+        print(f"calling : {func.__name__} with args {args_value} and kwargs {kwargs_value}")
+        return func(*args,**kwargs)
+    return wrapper
+
+
+@debug
+def greet(name,greeting="Hello"):
+    print(f"{greeting},{name}")
+
+greet("Modi Ji",greeting="Prime Minister of India") # calling : greet with args Modi Ji and kwargs greeting=Prime Minister of India
